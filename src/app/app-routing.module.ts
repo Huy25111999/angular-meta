becd5c2b3883093f59771bcead081199c7986691
Le Van Huy <vanhuy.le@wiz.com.sg>
2024-02-11 16:55:25 +0700

    user-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/trang-chu'},
   {path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)},
   {path: 'trang-chu', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)},
+  {path: 'users', loadChildren: () => import('./pages/user-management/user-management.module').then(m => m.UserManagementModule)},
 ];
 
 @NgModule({
